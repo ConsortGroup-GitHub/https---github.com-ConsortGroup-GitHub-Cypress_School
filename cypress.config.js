1c6f5b2b9c8f7af4c2eb03a392b47f2aa4415cbd
ConsortGroup-GitHub <GSD.DLG.DOSol.GitHub@consort-group.com>
2023-11-03 16:56:17 +0100

    Make reports great again

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,14 @@ const {downloadFile} = require('cypress-downloadfile/lib/addPlugin')
 module.exports = defineConfig({
   e2e: {
     watchForFileChanges: false,
+    "reporter":"mochawesome",
+    "reporterOptions": {
+      "charts": true,
+      "overwrite": false,
+      "html": true,
+      "json": true,
+      "reportDir": "cypress/report"
+    },
     defaultCommandTimeout: 3000, // global level timeout
     setupNodeEvents(on, config) {
       // implement node event listeners here
